6b728616c7cda16d65a02e906a57307222c52b3a
Dmitry Mozzherin <dmozzherin@gmail.com>
2025-03-12 15:42:35 +0000

    wip

---
+++ config/config_test.go
@@ -0,0 +1,20 @@
+package config_test
+
+import (
+	"testing"
+
+	"github.com/gnames/dwca/config"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestConfigDefault(t *testing.T) {
+	assert := assert.New(t)
+	conf := config.New()
+	assert.Contains(conf.RootPath, "dwca_go")
+
+	opts := []config.Option{
+		config.OptRootPath("test"),
+	}
+	conf = config.New(opts...)
+	assert.Equal("test", conf.RootPath)
+}
---
+++ interface.go
@@ -0,0 +1,4 @@
+package dwca
+
+type Archive interface {
+}
